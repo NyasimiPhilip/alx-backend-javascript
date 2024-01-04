dbfba9dc12b2ae48862509ecffa005e024024162
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2024-01-04 13:39:11 +0300

    Update

---
+++ 0x02-ES6_classes/10-loops.js
@@ -0,0 +1,12 @@
+export default function appendToEachArrayValue(array, appendString) {
+    // Create a new array to store modified values
+    const newArray = [];
+  
+    // Use for...of loop to iterate through the array
+    for (const value of array) {
+      // Modify each array element and push it to the new array
+      newArray.push(appendString + value);
+    }
+  
+    return newArray;
+  }
