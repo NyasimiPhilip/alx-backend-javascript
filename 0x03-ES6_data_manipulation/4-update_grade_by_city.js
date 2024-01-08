901910ef1ef2e6c0c45ed45309534051c16f7dd0
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2024-01-08 17:54:19 +0300

    Update

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    const studentsInCity = students.filter((student) => student.location === city);
+  
+    const newStudentsGrade = studentsInCity.map((student) => {
+      const studentCopy = student;
+      for (const grade of newGrades) {
+        if (grade.studentId === studentCopy.id) {
+          studentCopy.grade = grade.grade;
+          return studentCopy;
+        }
+      }
+      studentCopy.grade = 'N/A';
+      return studentCopy;
+    });
+    return newStudentsGrade;
+  }
