dbb623ea3dc20903cb260cd3f2433933a6868e55
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2024-01-10 08:43:40 +0300

    Update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -8,7 +8,7 @@ export default function getListStudents() {
   const listStudents = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena           ', location: 'San Francisco' },
   ];
 
   // Return the array of student objects.
